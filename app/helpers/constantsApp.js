c69e2233a65c3a0e3d10df8dae02e763f0ec9886
Hunter Rafuse <31886722+GhostPolymer@users.noreply.github.com>
2018-04-03 19:30:36 +0300

    Add files via upload

---
+++ app/helpers/constantsApp.js
@@ -0,0 +1,7 @@
+export const ADS_POSITION = 20;
+export const ADS_URL = '/ad/?r=';
+export const API_BASE_URL = '/api/';
+export const LIMIT = 20;
+export const LOADING_TEXT_END = 'Loading...';
+export const LOADING_TEXT_START = 'Loading.';
+export const SORT_OPTIONS = ['', 'size', 'price', 'id'];
